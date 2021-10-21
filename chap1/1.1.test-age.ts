d0a6e19036ef17961b9cc91148593bf1d54c6828
aminuolawalekan <mohammed.a@turing.com>
2021-10-21 16:31:41 +0100

    concluded chap1

---
+++ chap1/1.1.test-age.ts
@@ -0,0 +1,15 @@
+interface User {
+  name: string;
+  age: number;
+}
+function canDrive(usr: User) {
+  console.log("user is", usr.name);
+  if (usr.age >= 16) {
+    console.log("allow to drive");
+  } else {
+    console.log("do not allow to drive");
+  }
+}
+
+const tom: User = { name: "tom", age: 19 };
+canDrive(tom);
